71185a17dd81013762a707a926857386d04c5a9f
Liam <lcn.gevaerts@student.avans.nl>
2025-04-16 11:18:27 +0200

    might work now?

--- apps/data-api/webpack.config.js
+++ apps/data-api/webpack.config.js
@@ -1,29 +1,8 @@
-const { join } = require('path');
-const nodeExternals = require('webpack-node-externals');
+const { composePlugins, withNx } = require('@nx/webpack');
 
-module.exports = {
-  target: 'node',
-  mode: 'production', // voeg deze toe om de warning weg te halen
-  externals: [nodeExternals()],
-  entry: './apps/data-api/src/main.ts',
-  output: {
-    path: join(__dirname, '../../dist/apps/data-api'),
-    filename: 'main.js',
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-    alias: {
-      '@project/backend-dto': join(__dirname, '../../libs/backend/dto/src/index.ts'),
-      '@project/libs/shared/api': join(__dirname, '../../libs/shared/api/src/index.ts'),
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-};
+// Nx plugins for webpack.
+module.exports = composePlugins(withNx(), (config) => {
+  // Update the webpack config as needed here.
+  // e.g. `config.plugins.push(new MyPlugin())`
+  return config;
+});
